bb484782965e45a55aca720c6716e21e7c08b3b6
Mike Young <mike.young@pearcircuit.com>
2022-04-14 21:48:43 -0400

    Adjust env

--- content/src/components/Cio.js
+++ content/src/components/Cio.js
@@ -7,7 +7,7 @@ function Cio(){
     var s = document.getElementsByTagName('script')[0];
     t.async = true;
     t.id    = 'cio-forms-handler';
-    t.setAttribute('data-site-id', `{process.env.REACT_APP_CIO_KEY}`);
+    t.setAttribute('data-site-id', {process.env.REACT_APP_CIO_KEY});
     t.setAttribute('data-base-url', 'https://customerioforms.com');
 
     t.src = 'https://customerioforms.com/assets/forms.js';
