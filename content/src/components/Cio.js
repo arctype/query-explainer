3def0d93e60be4f6f0ffc121ce3d627e63d4350a
Mike Young <mike.young@pearcircuit.com>
2022-04-14 21:51:56 -0400

    modify cio integration script

--- content/src/components/Cio.js
+++ content/src/components/Cio.js
@@ -3,16 +3,18 @@ import {Component} from 'react'
 
 function Cio(){
 
-  var t = document.createElement('script');
-    var s = document.getElementsByTagName('script')[0];
+  (function() {
+    var t = document.createElement('script'),
+        s = document.getElementsByTagName('script')[0];
     t.async = true;
     t.id    = 'cio-forms-handler';
-    t.setAttribute('data-site-id', {process.env.REACT_APP_CIO_KEY});
+    t.setAttribute('data-site-id', 'a892e670cc1ee1c4e442');
     t.setAttribute('data-base-url', 'https://customerioforms.com');
 
     t.src = 'https://customerioforms.com/assets/forms.js';
 
     s.parentNode.insertBefore(t, s);
+  })();
 }
 
 export default Cio
